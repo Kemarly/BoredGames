334c5c4eed21093db24258ed24cab5cbb8e804bc
KMarly D <44208106+Kemarly@users.noreply.github.com>
2024-03-13 19:26:43 -0400

    zoom

--- app/src/main/java/com/example/boredgames/SplashScreen.kt
+++ app/src/main/java/com/example/boredgames/SplashScreen.kt
@@ -4,18 +4,31 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
-
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var zoom: Animation
+    private lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        zoom = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom)
+        img = findViewById(R.id.splash1)
+        img.startAnimation(zoom)
+
         Handler().postDelayed({
             val intent = Intent(this@SplashScreen, SplashScreen2::class.java)
             startActivity(intent)
-        }, 2000)
+            finish()
+        }, 5000)
+
     }
 }
