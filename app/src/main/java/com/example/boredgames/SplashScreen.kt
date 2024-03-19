f5237833221c8359f12145a9d748bb9eaa6eab20
KMarly D <44208106+Kemarly@users.noreply.github.com>
2024-03-19 03:11:36 -0400

    splash works

--- app/src/main/java/com/example/boredgames/SplashScreen.kt
+++ app/src/main/java/com/example/boredgames/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.example.boredgames
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -8,28 +7,58 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
 
     private lateinit var zoomInAnimation: Animation
+    private lateinit var zoomOutAnimation: Animation
+    private lateinit var fadeOutAnimation: Animation
     private lateinit var img: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(5000 )
-        installSplashScreen()
         setContentView(R.layout.activity_splash_screen)
 
         zoomInAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom)
+        zoomOutAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
+        fadeOutAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade)
         img = findViewById(R.id.splash1)
+
+        img.scaleX = 1.0f
+        img.scaleY = 1.0f
         img.startAnimation(zoomInAnimation)
 
-        Handler().postDelayed({
-            val intent = Intent(this@SplashScreen, SignupActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 5000) // Delay for 2 seconds
+        zoomInAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                img.startAnimation(fadeOutAnimation)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        fadeOutAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                img.setImageResource(R.drawable.splash2)
+                img.startAnimation(zoomOutAnimation)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        zoomOutAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val intent = Intent(this@SplashScreen, SignupActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
     }
 }
