32d03d63ace909513d833e5bcb2877d21b5e7651
KMarly D <44208106+Kemarly@users.noreply.github.com>
2024-05-13 04:58:55 -0400

    grid and time

--- app/src/main/java/com/example/boredgames/Sudoku.java
+++ app/src/main/java/com/example/boredgames/Sudoku.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,8 @@ public class Sudoku extends AppCompatActivity {
 
     ImageButton HomeButton;
     CountDownTimer timer;
+    private TextView[][] grid;
+    private int [][] answers;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -20,8 +23,15 @@ public class Sudoku extends AppCompatActivity {
         setContentView(R.layout.activity_sudoku);
 
         HomeButton = (ImageButton) findViewById(R.id.HomeButton);
-
         HomeButton.setOnClickListener(v -> GoHome());
+
+        grid = new TextView[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+            }
+        }
+        //makeGrid();
         resetTimer();
     }
 
@@ -34,11 +44,16 @@ public class Sudoku extends AppCompatActivity {
         timer = new CountDownTimer(Long.MAX_VALUE, 1000) {
             long elapsedTime = 0;
             @Override
-            public void onTick(long millisUntilFinished) {elapsedTime += 1000;}
-
+            public void onTick(long millisUntilFinished)
+            {
+                elapsedTime += 1000;
+                TextView timerTextView = findViewById(R.id.Time);
+                timerTextView.setText(String.valueOf(elapsedTime / 1000));
+            }
             @Override
             public void onFinish() { }
-
-        };
+        }.start();
     }
+    private void makeGrid(){}
+    private void userAnswers(TextView num){}
 }
